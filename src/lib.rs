2c4bdd684076f5d8b93c1d3e6e5018a2ce07aada
Nakanishi <45790603+Nakanishi123@users.noreply.github.com>
2024-05-23 00:26:43 +0900

    Add multiplication function and tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,22 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn mul(left: usize, right: usize) -> usize {
+    left * right
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn test_add() {
         let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_mul() {
+        let result = mul(2, 2);
         assert_eq!(result, 4);
     }
 }
